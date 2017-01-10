9a1a492c9b70bfd366e14642be47fdd3c312cbfc
Vincent Mo <ktmswzw@gmail.com>
2017-01-10 23:39:19 +0800

    slimLoader toast

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,12 +1,18 @@
 import {Component, ViewEncapsulation} from '@angular/core';
+import {SlimLoadingBarService} from "ng2-slim-loading-bar";
+import {Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError} from "@angular/router";
+import {ToastyService, ToastyConfig,ToastOptions, ToastData} from 'ng2-toasty';
+
 @Component({
   selector: 'pages',
   encapsulation: ViewEncapsulation.None,
-  styles: [],
+  styles: [require('./style-default.css')],
   template: `
     <ba-sidebar></ba-sidebar>
     <ba-page-top></ba-page-top>
     <div class="al-main">
+      <ng2-toasty [position]="'top-center'"></ng2-toasty>
+      <ng2-slim-loading-bar></ng2-slim-loading-bar>
       <div class="al-content">
         <ba-content-top></ba-content-top>
         <router-outlet></router-outlet>
@@ -29,10 +35,65 @@ import {Component, ViewEncapsulation} from '@angular/core';
 })
 export class Pages {
   title = "";
-  constructor() {
+  private sub: any;
+
+
+  constructor(private router: Router,
+              private slimLoader: SlimLoadingBarService,
+              private toastyService:ToastyService,
+              private toastyConfig: ToastyConfig) {
+
+    this.sub = this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        console.log('Loading start1');
+        this.slimLoader.start();
+      }
+      else if (event instanceof NavigationEnd ||
+        event instanceof NavigationCancel ||
+        event instanceof NavigationError) {
+        console.log('Loading complete1');
+        this.slimLoader.complete();
+        this.addToast();
+      }
+    }, (error: any) => {
+      console.log('Loading complete2');
+      this.slimLoader.complete();
+    });
+    this.toastyConfig.theme = 'default';
+  }
+
+  addToast() {
+    // Just add default Toast with title only
+    this.toastyService.default('Hi there');
+    // Or create the instance of ToastOptions
+    var toastOptions:ToastOptions = {
+      title: "My title",
+      msg: "The message",
+      showClose: true,
+      timeout: 5000,
+      theme: 'default',
+      onAdd: (toast:ToastData) => {
+        console.log('Toast ' + toast.id + ' has been added!');
+      },
+      onRemove: function(toast:ToastData) {
+        console.log('Toast ' + toast.id + ' has been removed!');
+      }
+    };
+    // Add see all possible types in one shot
+    this.toastyService.info(toastOptions);
+    this.toastyService.success(toastOptions);
+    this.toastyService.wait(toastOptions);
+    this.toastyService.error(toastOptions);
+    this.toastyService.warning(toastOptions);
   }
 
   ngOnInit() {
     this.title = '123123';
   }
+
+
+  ngOnDestroy(): any {
+    this.sub.unsubscribe();
+  }
+
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,9 +5,13 @@ import { routing }       from './pages.routing';
 import { NgaModule } from '../theme/nga.module';
 
 import { Pages } from './pages.component';
+import {ToastyModule} from "ng2-toasty";
+import {SlimLoadingBarModule} from "ng2-slim-loading-bar";
 
 @NgModule({
-  imports: [CommonModule, NgaModule, routing],
+  imports: [CommonModule, NgaModule, routing,
+    ToastyModule.forRoot(),
+    SlimLoadingBarModule.forRoot(),],
   declarations: [Pages]
 })
 export class PagesModule {
